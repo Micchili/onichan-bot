db2b99b59f6de50f0f345d475dbf6196cabd9645
Micchili <wot.dannbo@gmail.com>
2021-03-03 13:55:06 +0900

    神奈川県の天気を表示するコマンドを実装

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
-const { prefix, token , channel } = require('./config.json');
+const axios = require('axios');
+const { prefix, token, channel } = require('./config.json');
 
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('準備完了！');
+    console.log('準備完了！');
 });
 
 client.login(token);
@@ -12,28 +13,41 @@ client.login(token);
 client.on('message', message => {
     if (message.channel.name === channel) {
         const [command, ...args] = message.content.slice(prefix.length).split(' ')
-        if (message.content === `${prefix}`){
-           message.channel.send('なぁに？　お兄ちゃん。桃子のことが知りたかったら`!momoko help`を打ってね');
-        } 
+        if (message.content === `${prefix}`) {
+            message.channel.send('なぁに？　お兄ちゃん。桃子のことが知りたかったら`!momoko help`を打ってね');
+        }
         else if (message.content === `${prefix} ping`) {
             message.channel.send(`桃子だけど。\nServer Name: ${message.guild.name}\nBot Name: ${message.guild.me}`);
-        } 
+        }
         else if (message.content === `${prefix} mokomoko`) {
             message.channel.send('お兄ちゃん❓');
-        } 
+        }
         else if (message.content === `${prefix} help`) {
             message.channel.send("お兄ちゃんお姉ちゃんを支える素敵なbot、onichan-bot\n\nコマンド一覧\n\n`!momoko ping`\nonichan-botが動いているか確認出来ます。\n\n`!momoko ramenあなたが指定したい分数`\nラーメンタイマーです。10分まで指定できます。負の整数には対応していません\n例\n`!momoko ramen 4`\n`!momoko ramen 6`");
-        } 
+        }
+        else if (message.content === `${prefix} tenki`) {
+            axios.get('https://weather.tsukumijima.net/api/forecast/city/140010')
+                .then(function (response) {
+                    console.log(response.data);
+                    message.channel.send(`${response.data.forecasts[0].dateLabel}\n${response.data.forecasts[0].date}\n${response.data.forecasts[0].telop}\n\n${response.data.description.text}`);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    message.channel.send(`今はちょっとダメみたい…。他のコマンドを試してみてね。`);
+                })
+                .finally(function () {
+                });
+        }
         else if (message.content === `${prefix} ramen ${args[1]}`) {
             const seconds = Number(args[1])
             if (!isNaN(seconds)) {
-                if (seconds <= 10 ) {
+                if (seconds <= 10) {
                     if (seconds >= 0) {
                         const RAMEN_TIMER = 60000
                         message.channel.send(`ラーメンタイマーを${seconds}分に設定したよ❕\n${seconds}分後、あなた宛てにメンションが来ます`);
                         setTimeout(() => {
                             message.reply(`${seconds}分たったよ、あったかいうちに食べてね。`);
-                          }, seconds * RAMEN_TIMER)
+                        }, seconds * RAMEN_TIMER)
                     }
                     else {
                         message.channel.send("マイナスの値を使わないで❕　使い方が分からなかったら`!momoko help`を打ってね");
