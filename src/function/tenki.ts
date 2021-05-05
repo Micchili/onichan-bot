ea061163e6cb1028fd29ec1a413e7e5d36640c61
Micchili <wot.dannbo@gmail.com>
2021-05-06 00:28:29 +0900

    [UPDATE] 気象庁のデータをそのまま使えるようにした

--- src/function/tenki.ts
+++ src/function/tenki.ts
@@ -1,30 +1,18 @@
 import Discord from "discord.js"
 import axios from 'axios'
 
-export async function tenki(isDate?: string) {
-    const response = await axios.get('https://weather.tsukumijima.net/api/forecast/city/140010')
-    if(response) {
-        if(!isDate) {
-            const embed = new Discord.MessageEmbed()
-                .setTitle(response.data.forecasts[0].dateLabel + ' ' + response.data.forecasts[0].date)
-                .setImage(response.data.forecasts[0].image.url)
-                .setDescription(response.data.forecasts[0].telop)
-                .setThumbnail(response.data.copyright.image.url)
-                .addField("温度" , "最低温度:" + response.data.forecasts[0].temperature.min.celsius + "\n最高温度:" + response.data.forecasts[0].temperature.max.celsius)
-                .addField("天気の詳細" , response.data.forecasts[0].detail.weather + "\n\n" + response.data.description.bodyText)
-            return embed
-        } else if(isDate === "tomorrow") {
-            const embed = new Discord.MessageEmbed()
-                .setTitle(response.data.forecasts[1].dateLabel+ ' ' + response.data.forecasts[1].date)
-                .setImage(response.data.forecasts[1].image.url)
-                .setDescription(response.data.forecasts[1].telop)
-                .setThumbnail(response.data.copyright.image.url)
-                .addField("温度" , "最低温度:" + response.data.forecasts[1].temperature.min.celsius + "\n最高温度:" + response.data.forecasts[1].temperature.max.celsius)
-                .addField("天気の詳細" , response.data.forecasts[1].detail.weather + "\n\n" + response.data.description.bodyText)
-            return embed
-        } else {
-            return "そんなコマンドは無いよ。もしかして=>`tomorrow`"
-        }
+export async function tenki() {
+    const pathCode = 140000
+    const response = await axios.get(`https://www.jma.go.jp/bosai/forecast/data/forecast/${pathCode}.json`) as any
+    if (response) {
+        const kionMin = response.data[0].timeSeries[2].areas[0].temps[0]
+        const kionMax = response.data[0].timeSeries[2].areas[0].temps[1]
+        const weathersText = response.data[0].timeSeries[0].areas[1].weathers[0]
+        const embed = new Discord.MessageEmbed()
+            .setTitle("神奈川県横浜市" + response.data[0].reportDatetime)
+            .addField("温度", "最低温度:" + kionMin + "\n最高温度:" + kionMax)
+            .addField("天気の詳細", weathersText)
+        return embed
     } else {
         return "今はちょっとダメみたい…。他のコマンドを試してみてね。"
     }
