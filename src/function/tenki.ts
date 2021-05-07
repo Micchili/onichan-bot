aea91bfbae66cc0796ff86934c4e7bfddb25e420
Micchili <wot.dannbo@gmail.com>
2021-05-07 20:58:20 +0900

    [UPDATE] jsonで表示させる内容を変数に入れた

--- src/function/tenki.ts
+++ src/function/tenki.ts
@@ -8,10 +8,13 @@ export async function tenki() {
         const kionMin = response.data[0].timeSeries[2].areas[0].temps[0]
         const kionMax = response.data[0].timeSeries[2].areas[0].temps[1]
         const weathersText = response.data[0].timeSeries[0].areas[1].weathers[0]
+        const todayTime = response.data[0].reportDatetime
+
         const embed = new Discord.MessageEmbed()
-            .setTitle("神奈川県横浜市" + response.data[0].reportDatetime)
+            .setTitle("神奈川県横浜市" + todayTime)
             .addField("温度", "最低温度:" + kionMin + "\n最高温度:" + kionMax)
             .addField("天気の詳細", weathersText)
+
         return embed
     } else {
         return "今はちょっとダメみたい…。他のコマンドを試してみてね。"
