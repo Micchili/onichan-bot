a7d0e9bd06dcad8807de4f484d8829de67fcdc17
hairihou <38448411+hairihou@users.noreply.github.com>
2021-05-10 05:37:04 +0900

    fix: tenkiとramenのリファクタリング

--- src/function/ramen.ts
+++ src/function/ramen.ts
@@ -1,36 +1,41 @@
-export function ramen(second: number): {massage: string , correct?: boolean} {
-    if (!isNaN(second)) {
-        if (second <= 15) {
-            if (second >= 0) {
-                return {
-                    massage: `ラーメンタイマーを${second}分に設定したよ❕\n${second}分後、あなた宛てにメンションが来ます`,
-                    correct: true
-                }
-            }
-            else {
-                return {
-                    massage: "マイナスの値を使わないで❕　使い方が分からなかったら`!momoko help`を打ってね"
-                }
-            }
-        }
-        else {
-            return {
-                massage: "そんなに待ったら伸びちゃうでしょ！"
-            }
-        }
-    }
-    else {
-        return {
-            massage: "数字を指定してね。"
-        }
-    }
+interface RamenResult {
+  message: string;
+  success: boolean;
 }
 
-export function timer(second: number): Promise<string> {
-    const RAMEN_TIMER = 60000
-    return new Promise(() => {
-        setTimeout(() => {
-            `${second}分たったよ、あったかいうちに食べてね。`
-        }, second * RAMEN_TIMER)
-    })
+export function ramen(arg: string): RamenResult {
+  const minutes = parseInt(arg);
+
+  if (isNaN(minutes)) {
+    return {
+      message: '数字を指定してね。',
+      success: false
+    };
+  }
+
+  if (minutes > 15) {
+    return {
+      message: 'そんなに待ったら伸びちゃうでしょ！',
+      success: false
+    };
+  }
+
+  const message = minutes ?
+    `ラーメンタイマーを${minutes}分に設定したよ❕\n${minutes}分後、あなた宛てにメンションが来ます` :
+    'マイナスの値を使わないで❕　使い方が分からなかったら`!momoko help`を打ってね';
+  return {
+    message: message,
+    success: Boolean(minutes)
+  };
+}
+
+/**
+ * タイマー
+ * @param { string } arg 計測する時間(分)
+ * @returns { string } 完了通知
+ */
+export async function timer(arg: string): Promise<string> {
+  const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+  await sleep(parseInt(arg) * 60000);
+  return `${arg}分たったよ、あったかいうちに食べてね。`;
 }
--- src/function/tenki.ts
+++ src/function/tenki.ts
@@ -1,29 +1,52 @@
-import Discord from "discord.js"
-import axios from 'axios'
+import { MessageEmbed } from 'discord.js';
+import axios from 'axios';
 
-export async function tenki(areaCode?: string) {
-    let pathCode
-    if (areaCode) {
-        pathCode = areaCode
-    }
-    else {
-        pathCode = '140000'
-    }
-    try {
-        const response: any = await axios.get(`https://www.jma.go.jp/bosai/forecast/data/forecast/${pathCode}.json`)
-        const kionMin = response.data[0].timeSeries[2].areas[0].temps[0]
-        const kionMax = response.data[0].timeSeries[2].areas[0].temps[1]
-        const weathersText = response.data[0].timeSeries[0].areas[1].weathers[0]
-        const todayTime = response.data[0].reportDatetime
-        const town = response.data[0].publishingOffice
+interface TenkiResult {
+  message: MessageEmbed | string;
+  success: boolean;
+}
 
-        const embed = new Discord.MessageEmbed()
-            .setTitle(town + todayTime)
-            .addField("温度", "最低温度:" + kionMin + "\n最高温度:" + kionMax)
-            .addField("天気の詳細", weathersText)
+export async function tenki(areaCode = '140000'): Promise<TenkiResult> {
+  if (areaCode.length !== 6) {
+    return {
+      message: 'エリアコードが間違ってるよ',
+      success: false
+    };
+  }
 
-        return embed
-    } catch (error) {
-        return "サーバーがエラーを起こしているか、エリアコードが間違ってるよ\n" + `${error}`
-    }
+  try {
+    const url = `https://www.jma.go.jp/bosai/forecast/data/forecast/${areaCode}.json`;
+    const response = await axios.get(url);
+    const kionMin = response.data[0].timeSeries[2].areas[0].temps[0];
+    const kionMax = response.data[0].timeSeries[2].areas[0].temps[1];
+    const weathersText = response.data[0].timeSeries[0].areas[1].weathers[0];
+    const todayTime = response.data[0].reportDatetime;
+    const town = response.data[0].publishingOffice;
+
+    const embedMessage = new MessageEmbed(
+      {
+        title: town + todayTime,
+        fields: [
+          {
+            name: '温度',
+            value: `最低温度:${kionMin}\n最高温度:${kionMax}`
+          },
+          {
+            name: '天気の詳細',
+            value: weathersText
+          }
+        ]
+      }
+    );
+
+    return {
+      message: embedMessage,
+      success: true
+    };
+  } catch (error) {
+    return {
+      message: `サーバーがエラーを起こしているか、エリアコードが間違ってるよ\n${error}`,
+      success: false
+    };
+  }
 }
