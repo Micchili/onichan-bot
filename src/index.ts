a64320998a59742678a754aa6feea3c91cd30c87
hairihou <38448411+hairihou@users.noreply.github.com>
2021-05-11 22:05:54 +0900

    chore: distの削除とフォーマット

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const COMMAND_LIST = {
   RAMEN: 'ramen',
   MOKOMOKOK: 'mokomoko',
   OUEN: 'ouen',
-  AREA: 'area'
+  AREA: 'area',
 };
 
 const client = new Client();
@@ -22,11 +22,11 @@ const client = new Client();
 client.once('ready', () => console.log('準備完了！'));
 client.on('message', async (message: Message): Promise<void> => {
   if (message.channel.id === channel) {
-    const [command, parameter, operator] = message.content.split(' ')
+    const [command, parameter, operator] = message.content.split(' ');
     if (command === prefix) {
       switch (parameter) {
         case COMMAND_LIST.HELP: {
-          message.channel.send(help())
+          message.channel.send(help());
           break;
         }
         // ramen
@@ -46,53 +46,57 @@ client.on('message', async (message: Message): Promise<void> => {
           break;
         }
         case COMMAND_LIST.MOKOMOKOK: {
-          message.channel.send('お兄ちゃん❓')
-          break
+          message.channel.send('お兄ちゃん❓');
+          break;
         }
         case COMMAND_LIST.OUEN: {
-          const channel = message.member?.voice.channel
+          const channel = message.member?.voice.channel;
           if (channel) {
-            channel.join()
-              .then(connection => {
-                const dispatcher = connection.play(fs.createReadStream('src/voice/ouen.mp3'))
-                dispatcher.on('start', () => {
-                  dispatcher.setVolume(0.5)
-                  console.log('再生成功')
-                  message.channel.send(`フレー❕　フレー❕　お兄ちゃん❕`);
-                })
-
-                dispatcher.on('error', (err) => {
-                  console.log(err)
-                  message.channel.send(`今はちょっとダメみたい…他のコマンドを試してみてね。`);
-                  connection.disconnect();
-                })
-
-                dispatcher.on('finish', () => {
-                  console.log('再生終わり');
-                  connection.disconnect();
-                });
+            channel.join().then((connection) => {
+              const dispatcher = connection.play(
+                fs.createReadStream('src/voice/ouen.mp3')
+              );
+              dispatcher.on('start', () => {
+                dispatcher.setVolume(0.5);
+                console.log('再生成功');
+                message.channel.send(`フレー❕　フレー❕　お兄ちゃん❕`);
               });
+
+              dispatcher.on('error', (err) => {
+                console.log(err);
+                message.channel.send(
+                  `今はちょっとダメみたい…他のコマンドを試してみてね。`
+                );
+                connection.disconnect();
+              });
+
+              dispatcher.on('finish', () => {
+                console.log('再生終わり');
+                connection.disconnect();
+              });
+            });
+          } else {
+            message.channel.send('どこかのボイスチャンネルに入ってね❕');
           }
-          else {
-            message.channel.send('どこかのボイスチャンネルに入ってね❕')
-          }
-          break
+          break;
         }
-        case COMMAND_LIST.AREA: {
-          if (operator) {
-            message.channel.send(await areaCode(operator as Pref))
+        case COMMAND_LIST.AREA:
+          {
+            if (operator) {
+              message.channel.send(await areaCode(operator as Pref));
+            } else {
+              message.channel.send('検索したい県や地方を入力してね');
+            }
           }
-          else {
-            message.channel.send('検索したい県や地方を入力してね')
-          }
-        }
-          break
+          break;
         default:
-          message.channel.send('そんなコマンドは無いよ。桃子のことが知りたかったら`!momoko help`を打ってね')
-          break
+          message.channel.send(
+            'そんなコマンドは無いよ。桃子のことが知りたかったら`!momoko help`を打ってね'
+          );
+          break;
       }
     }
   }
-})
+});
 
-client.login(token)
+client.login(token);
