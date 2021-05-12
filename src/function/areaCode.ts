28d2ae53a49546f1ffd5e8c6fa98520bb210ac74
Micchili <wot.dannbo@gmail.com>
2021-05-12 21:11:39 +0900

    [FIX] as anyを削除

--- src/function/areaCode.ts
+++ src/function/areaCode.ts
@@ -2,15 +2,15 @@ import axios from 'axios';
 import { Pref } from '../pref';
 
 export async function areaCode(pref: Pref): Promise<any> {
-  const ereaCodeJson = (await axios.get(
-    'http://www.jma.go.jp/bosai/common/const/area.json'
-  )) as any;
-
-  if (ereaCodeJson) {
-    for (const key in ereaCodeJson.data.offices) {
-      if (ereaCodeJson.data.offices[key].name == pref) {
-        return pref + 'のエリアコード:' + key;
-      }
+    const url = 'http://www.jma.go.jp/bosai/common/const/area.json'
+    const ereaCodeJson = await axios.get(url)
+    if (ereaCodeJson) {
+        for (const key in ereaCodeJson.data.offices) {
+            if (ereaCodeJson.data.offices[key].name == pref) {
+                return(pref + "のエリアコード:" + key)
+            }
+        }
+        return("該当するエリアコードが無かったよ。")
     }
     return '該当するエリアコードが無かったよ。';
   }
